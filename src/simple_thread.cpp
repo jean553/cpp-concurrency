623821a0eb922a2fbf1fae338f6911cfdaa06597
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-14 00:07:38 +0200

    add sleep into simple_thread threaded procedure

--- src/simple_thread.cpp
+++ src/simple_thread.cpp
@@ -10,7 +10,8 @@
  */
 void displayFromTo(
     const unsigned short& start,
-    const unsigned short& end
+    const unsigned short& end,
+    const unsigned short& sleepTime
 ) {
 
     for(
@@ -19,6 +20,10 @@ void displayFromTo(
         i += 1
     ) {
         std::cout << i << std::endl;
+
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds(sleepTime)
+        );
     }
 }
 
@@ -28,13 +33,27 @@ void displayFromTo(
 void runSimpleThread() {
 
     constexpr unsigned short FIRST_THREAD_START {0};
-    constexpr unsigned short FIRST_THREAD_END {100};
+    constexpr unsigned short FIRST_THREAD_END {10};
+    constexpr unsigned short FIRST_SLEEP_TIME {1000};
 
-    std::thread thread(
+    std::thread firstThread(
         displayFromTo,
         FIRST_THREAD_START,
-        FIRST_THREAD_END
+        FIRST_THREAD_END,
+        FIRST_SLEEP_TIME
     );
 
-    thread.join();
+    constexpr unsigned short SECOND_THREAD_START {100};
+    constexpr unsigned short SECOND_THREAD_END {110};
+    constexpr unsigned short SECOND_SLEEP_TIME {500};
+
+    std::thread secondThread(
+        displayFromTo,
+        SECOND_THREAD_START,
+        SECOND_THREAD_END,
+        SECOND_SLEEP_TIME
+    );
+
+    firstThread.join();
+    secondThread.join();
 }
