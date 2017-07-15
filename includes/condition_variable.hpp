ab706eaea0bf56c7cecd618c7612437038ca70bc
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-15 19:33:41 +0200

    define one thread that modifies a condition variable in order to notify other threads

--- includes/condition_variable.hpp
+++ includes/condition_variable.hpp
@@ -2,6 +2,17 @@
  * @brief condition variable usage example
  */
 
+#include <condition_variable>
+
+/**
+ * @brief a first thread function;
+ * waits three seconds, update the condition variable
+ * and waits three other seconds
+ *
+ * @param reference to the condition variable to use to notify another thread
+ */
+void firstThreadFunction(std::condition_variable& cv);
+
 /**
  * @brief spaws two threads; one thread communicates
  * with the other using condition variable
--- src/condition_variable.cpp
+++ src/condition_variable.cpp
@@ -4,8 +4,44 @@
 
 #include "condition_variable.hpp"
 
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+/**
+ *
+ */
+void firstThreadFunction(std::condition_variable& cv) {
+
+    constexpr unsigned short SLEEP_TIME {3000};
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(SLEEP_TIME)
+    );
+
+    std::cout << "notify thread" << std::endl;
+
+    cv.notify_one();
+
+    std::cout << "thread notified" << std::endl;
+
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(SLEEP_TIME)
+    );
+
+    std::cout << "terminate first thread" << std::endl;
+}
+
 /**
  *
  */
 void functionWithConditionVariable() {
+
+    std::condition_variable cv;
+
+    std::thread firstThread(
+        firstThreadFunction,
+        std::ref(cv)
+    );
+
+    firstThread.join();
 }
