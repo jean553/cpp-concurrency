a26d6fbd151568ab288078f3ac24bb27a1c0b97b
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-15 11:43:29 +0200

    call the join vs detach script from the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "simple_thread.hpp"
 #include "simple_task.hpp"
 #include "thread_local_variables.hpp"
+#include "join_vs_detach.hpp"
 
 #include <iostream>
 
 int main()
 {
-    runFunctionWithThreadLocal();
+    executeThreadWithJoin();
 
     return EXIT_SUCCESS;
 }
